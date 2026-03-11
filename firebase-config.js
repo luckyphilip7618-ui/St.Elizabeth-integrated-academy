f76cdcaa4db133c9c335f1d719e78639ba4085f5
luckyphilip7618-ui <luckyphilip7618@gmail.com>
2026-03-11 08:18:33 +0300

    Add Firebase configuration and initialization

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDQuLtsxX3TVSIGgaLKdaWO7oe6trd3ZdY",
+  authDomain: "st-elizabeth-portal.firebaseapp.com",
+  projectId: "st-elizabeth-portal",
+  storageBucket: "st-elizabeth-portal.firebasestorage.app",
+  messagingSenderId: "126524490495",
+  appId: "1:126524490495:web:6426e727f5fe5371cf82ca"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
